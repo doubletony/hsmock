fef25cf922b79666a45bd64be555d1ded48262d4
doubletony <doubletony@gmail.com>
2015-08-22 19:47:17 -0700

    Implement the basic stuff.

--- src/hs.js
+++ src/hs.js
@@ -1,10 +1,30 @@
+var defaultValue = function(arg, value) {
+	return typeof arg !== 'undefined' ?  arg : value;
+};
+
+var Card = function(attack, health, cost, name) {
+	this.attack = defaultValue(attack, -1);
+	this.health = defaultValue(attack, -1);
+	this.cost = defaultValue(cost, -1);
+	this.name = defaultValue(name, '');
+};
 
 var PlayerSet = function(user) {
 	this.hero = null;
-	this.cards = ['minion card', 'spell card', 'lucky coin'];
+	this.cards = [new Card(1,1,1,'Minion 1'),
+		new Card(1,3,2,'Minion 2'),
+		new Card(0,4,1,'Minion 3')];
 	this.battlefiled = [];
 	this.mana = 0;
 	this.user = user;
+};
+
+var readlineSync = require('readline-sync');
+
+var Minion = function(attack, health, name) {
+	this.attack = defaultValue(attack, -1);
+	this.health = defaultValue(health, -1); 
+	this.name = defaultValue(name, 'no name');
 };
 
 var Game = function(){
@@ -12,30 +32,54 @@ var Game = function(){
 };
 
 PlayerSet.prototype.getCommand = function(game) {
-	for (var i = this.cards.length - 1; i >= 0; i--) {
-		console.log('Play card ' + this.cards[i]);
+	var actionCount = 1;
+	for (var i = 0; i < this.cards.length; i++) {
+		console.log(actionCount + ': Play card ' + this.cards[i].name);
+		actionCount += 1;
 	};
-}
+	// Show commands for minions that can attack this turn.
+	for (var i = 0; i < this.battlefiled.length; i++) {
+		console.log(actionCount + ': Attack by minion ' + this.battlefiled[i].name);
+		actionCount += 1;
+	};
+
+    console.log(actionCount + ': Pass.');
+	var command = readlineSync.question('Choose an action :');
+	return command;
+};
 
 PlayerSet.prototype.exectue = function(game){
 	/* body... */
-	this.getCommand(game);
+	var command = this.getCommand(game);
+	this.battlefiled.push(new Minion(1, 3, 'system test minion.'));
+	return command != 'quit';
 };
 
-Game.prototype.isEnd = function () {
+Game.prototype.isEnd = function() {
 	return false;
-}
+};
+
+Game.prototype.updateBattleField = function() {
+	var battlefiled1 = this.playerSets[0].battlefiled;
+	var battlefiled2 = this.playerSets[1].battlefiled;
+	console.log(battlefiled1);
+	console.log(battlefiled2);
+};
 
 Game.prototype.run = function(){
 	var playerId = 0;
 	while (!this.isEnd()) {
 		var player = this.playerSets[playerId];
 		console.log(player.user + "'s turn.");	
-		player.exectue();
+		if (!player.exectue()) {
+			break;
+		}
 		playerId = (playerId + 1) % (this.playerSets.length);
+		this.updateBattleField();
 	};	
 };
 
 var aGame = new Game();
 
 aGame.run();
+
